3e51c284684f93f92dfb8626463fb6f37641aded
Tomas Vai <tomasvai21@gmail.com>
2024-03-05 19:12:13 -0300

    commit

--- src/Components/ItemDetailContainer.jsx
+++ src/Components/ItemDetailContainer.jsx
@@ -1,10 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { getProductById } from '../api/asyncMocks';
+import ItemQuantitySelector from './ItemQuantitySelector';
+import Description from './Description';
+import AddItemButton from './AddItemButton';
+import { useCart } from '../CartContext';
 
 const ItemDetailContainer = () => {
   const { id } = useParams();
   const [product, setProduct] = useState(null);
+  const [quantity, setQuantity] = useState(1);
+  const { addToCart } = useCart();
 
   useEffect(() => {
     const fetchProduct = async () => {
@@ -19,6 +25,15 @@ const ItemDetailContainer = () => {
     fetchProduct();
   }, [id]);
 
+  const handleQuantityChange = (newQuantity) => {
+    setQuantity(newQuantity);
+  };
+
+  const handleAddToCart = () => {
+    addToCart(product, quantity);
+    console.log(`Added ${quantity} ${product.title} to cart`);
+  };
+
   return (
     <div>
       <h2>Detalles del Producto</h2>
@@ -27,6 +42,9 @@ const ItemDetailContainer = () => {
           <h2>{product.title}</h2>
           <img src={product.image} alt={product.title} />
           <p>{`Precio: $${product.price}, Stock: ${product.stock}`}</p>
+          <ItemQuantitySelector quantity={quantity} onChange={handleQuantityChange} />
+          <Description description={product.description} />
+          <AddItemButton onClick={handleAddToCart} />
         </div>
       )}
     </div>
